73b18b1433d1a1a2196787fc4e25f002c39a0d7b
developerjrts <developer.jrts@gmail.com>
2026-08-27 00:04:58 +0530

    LP

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,9 +7,7 @@ async function bootstrap() {
   const port = process.env.PORT ?? 5001;
   app.enableCors({
     origin: [
-      "http://localhost:3000/",
       "http://localhost:3000",
-      "https://ai-resume-intelligence-woad.vercel.app/",
       "https://ai-resume-intelligence-woad.vercel.app"
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
