0d17f7c7f9f15282c8eaecb7291e660acdc73d5a
Kamble Rutik <developer.jrts@gmail.com>
2026-08-27 00:53:52 +0530

    Update api.ts

--- frontend/api/api.ts
+++ frontend/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://ai-resume-intelligence-5jm4.onrender.com"
+const url = "https://ai-resume-intelligence-5jm4.onrender.com"
 // const url = "http://localhost:5000"
 const api = axios.create({
     baseURL: url,
