897c40bb84e06a3b1ab60f7fb7213042cb2947f7
developerjrts <developer.jrts@gmail.com>
2026-08-27 17:52:54 +0530

    URL fix

--- frontend/api/api.ts
+++ frontend/api/api.ts
@@ -1,12 +1,8 @@
 import axios from "axios";
 
-<<<<<<< HEAD
-export const url = "http://ai-resume-intelligence-5jm4.onrender.com"
-// export const url = "http://localhost:5000"
-=======
 const url = "https://ai-resume-intelligence-5jm4.onrender.com"
 // const url = "http://localhost:5000"
->>>>>>> 0d17f7c7f9f15282c8eaecb7291e660acdc73d5a
+
 const api = axios.create({
     baseURL: url,
     withCredentials: true
